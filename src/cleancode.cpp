3b9c4204ae2024b8e05861e481fe9cd31761815b
Deepak PK Shetty (MS/EBB4-ETAS-VOS) <dsh8cob@bosch.com>
2024-11-14 10:20:56 +0530

    Sample code

---
+++ src/cleancode.cpp
@@ -0,0 +1,40 @@
+//============================================================================
+// Name        : cleancode.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class FindLetter
+{
+public:
+	virtual void findAndPrintStringsContainingLetter(const string arr[], int size, char target) {
+	    bool found = false;
+	    for (int i = 0; i < size; ++i) {
+	        if (arr[i].find(target) != string::npos) {
+	            cout << "The letter '" << target << "' is found in the string: " << arr[i] << endl;
+	            found = true;
+	        }
+	    }
+	    if (!found) {
+	        cout << "The letter '" << target << "' is not found in any string in the array." << endl;
+	    }
+	}
+};
+
+
+int main() {
+
+	std::string arry[] = {"Bosch", "BGSW", "Test", "Deepak", "Shetty"};
+	FindLetter find_s;
+
+	int arraySize = sizeof(arry) / sizeof(arry[0]);
+	find_s.findAndPrintStringsContainingLetter(arry, arraySize, 'S');
+
+	return 0;
+}
