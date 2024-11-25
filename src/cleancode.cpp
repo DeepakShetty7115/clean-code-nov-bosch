2271814c8aaa34392803bccb6a61cc97a980d11b
Deepak PK Shetty (MS/EBB4-ETAS-VOS) <dsh8cob@bosch.com>
2024-11-25 09:31:09 +0530

    Factory

--- src/cleancode.cpp
+++ src/cleancode.cpp
@@ -1,102 +1,107 @@
-// Online C++ compiler to run C++ program online
-
+#include <functional>
+#include <unordered_map>
 #include <iostream>
+#include <memory>
+
 using namespace std;
 
-class RC {
-	string vehicleRegNo;
+// Abstract base class for Jobs
+class Job {
 public:
-	~RC() {
-		cout << "RC Destructed" << endl;
-	}
-	RC() {
-		cout << "RC Constructed" << endl;
-	}
+	virtual ~Job() = default;
+	virtual bool run() = 0;
 };
 
-class TPMS {
-public :
-	virtual string GetTPMValue() = 0;
-
-protected:
-
-	~TPMS() {
-		cout << "TPMS Destructed" << endl;
-	}
-	TPMS() {
-		cout << "TPMS Constructed" << endl;
-	}
-};
-
-class NexDigitronTPMS : public TPMS {
+// Concrete Job implementations
+class Spin: public Job {
 public:
-	string GetTPMValue() {
-		return "NexDigitronTPMS";
-	}
-
-	~NexDigitronTPMS() {
-		cout << "NexDigitronTPMS Destructed" << endl;
-	}
-	NexDigitronTPMS() {
-		cout << "NexDigitronTPMS Constructed" << endl;
+	bool run() override {
+		cout << "Spin";
+		return true;
 	}
 };
-class BoschTPMS : public TPMS {
+
+class Slide: public Job {
 public:
-	~BoschTPMS() {
-		cout << "BoschTPMS Destructed" << endl;
-	}
-	BoschTPMS() {
-		cout << "BoschTPMS Constructed" << endl;
-	}
-	string GetTPMValue() {
-		return "BoschTPMS";
+	bool run() override {
+		cout << "Slide";
+		return true;
 	}
 };
 
-class Engine {
-
+class Hop: public Job {
 public:
-	~Engine() {
-		cout << "Engine Destructed" << endl;
-	}
-	Engine() {
-		cout << "Engine Constructed" << endl;
+	bool run() override {
+		cout << "Hop";
+		return true;
 	}
 };
 
-class Car {
-	Engine engine;
-	TPMS *tpms;
-	string regNumber;
+// Job Factory
+class JobFactory {
 public:
-	Car(TPMS *ptr) :
-			tpms { ptr } {
-		cout << "Car Constructed" << endl;
+	using JobCreator = function<unique_ptr<Job>()>;
+
+	static JobFactory& getInstance() {
+		static JobFactory instance;
+		return instance;
 	}
-	~Car() {
-		cout << "Car Destructed" << endl;
+
+	void registerJob(unsigned int type, JobCreator creator) {
+		creators[type] = creator;
 	}
-	void installTPMS(TPMS *ptr) {
-		tpms = ptr;
+
+	unique_ptr<Job> createJob(unsigned int type) const {
+		auto it = creators.find(type);
+		if (it != creators.end()) {
+			return it->second();
+		} else {
+			throw runtime_error("Unknown job type");
+		}
 	}
+
+private:
+	unordered_map<unsigned int, JobCreator> creators;
+
+	JobFactory() = default;
+	~JobFactory() = default;
+
+	// Disallow copying and assigning
+	JobFactory(const JobFactory&) = delete;
+	JobFactory& operator=(const JobFactory&) = delete;
 };
 
-void buildCar(BoschTPMS *ptr) {
-	Car alto { ptr };
-}
-void buildCar(NexDigitronTPMS *ptr) {
-	Car swift { ptr };
+// Icon class using the JobFactory to create Jobs
+class Icon {
+public:
+	Icon(unsigned value) :
+			subtype(value) {
+	}
+
+	bool move() {
+		return JobFactory::getInstance().createJob(subtype)->run();
+	}
+
+	bool flair() {
+		return JobFactory::getInstance().createJob(subtype)->run();
+	}
+
+private:
+	unsigned subtype;
+};
+
+void registerJobs(JobFactory& factory) {
+    factory.registerJob(1, []() { return make_unique<Spin>(); });
+    factory.registerJob(2, []() { return make_unique<Slide>(); });
+    factory.registerJob(3, []() { return make_unique<Hop>(); });
 }
 
 int main() {
+	JobFactory& factory = JobFactory::getInstance();
+	    registerJobs(factory);
 
-	// Write C++ code here
-	BoschTPMS bTpms;
-	NexDigitronTPMS ndTpms;
-	buildCar(&bTpms);
-	buildCar(&ndTpms);
-
+	Icon icon(2);
+	icon.move();
+	icon.flair();
 	return 0;
-
 }
